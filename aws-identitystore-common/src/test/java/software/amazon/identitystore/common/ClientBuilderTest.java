544965d46acf41ab4ee43eda33bd51e1d1b87c00
Sandesh Timilsina <stimils@amazon.com>
2022-09-14 10:21:57 -0700

    Updated ClientBuilderTest to reflect IdentityStoreClient name

--- aws-identitystore-common/src/test/java/software/amazon/identitystore/common/ClientBuilderTest.java
+++ aws-identitystore-common/src/test/java/software/amazon/identitystore/common/ClientBuilderTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ClientBuilderTest {
 
     @Test
-    public void testCreateCloudFrontClient() {
+    public void testCreateIdentityStoreClient() {
         IdentitystoreClient client = ClientBuilder.getClient();
         assertThat(client).isNotNull();
     }
